c78b8f44817cbb56a463c017598e8432dda2ba0d
sday04 <sd120@wellesley.edu>
2026-09-18 13:59:47 -0400

    9/18 in-class progress

---
+++ test.java
@@ -0,0 +1,12 @@
+public static String removeChar(String s, char ch){
+    if (s.length() == 0){
+        return 
+    }
+    else {
+        if string[0] == ch;
+            print(--)
+        else{
+            
+        }
+    }
+}
